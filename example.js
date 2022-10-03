88095c2864178fd1c60d0db2e88da64ba47417b1
UpendraReddy7675 <upendrareddypolu@gmail.com>
2022-10-03 09:35:06 +0530

    fileSystem

--- example.js
+++ example.js
@@ -1,12 +1,4 @@
-let emp={
-    name:"upendra",
-    age:23,
-    study:{
-    Btech:"MVR College of Engineering and Technology",
-    Inter:"sriChaitanya jr College",
-    }
-};
-
-let {name,age,study}=emp;
-console.log(emp);
-console.log(emp.study);
+var fs = require('fs');
+fs.readFile('array.js','utf8',function(err,data){
+    console.log(data);
+});
